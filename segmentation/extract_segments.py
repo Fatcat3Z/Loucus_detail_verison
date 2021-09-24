1fe15a66b621d60dc99696cc7e31388d0b00f8e1
Fatcat3Z <hitzhongyu@163.com>
2021-09-24 09:38:52 +0800

    extract_segments_2d

--- segmentation/extract_segments.py
+++ segmentation/extract_segments.py
@@ -6,20 +6,27 @@ import pcl
 from pcl import pcl_visualization
 import os
 import sys
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils.kitti_dataloader import visualize_scan_open3d
 from utils.kitti_dataloader import visualiza_scan_clouds
+from utils.kitti_dataloader import range_projection_points
+from utils.kitti_dataloader import range_projection_colorseg
+
 # 提取欧几里德点云簇
 def extract_cluster_indices(cloud_filtered, seg_params):
-    tree = cloud_filtered.make_kdtree()      
+    tree = cloud_filtered.make_kdtree()
     ec = cloud_filtered.make_EuclideanClusterExtraction()
-    ec.set_ClusterTolerance(seg_params['c_tolerence']) 
+    ec.set_ClusterTolerance(seg_params['c_tolerence'])
     ec.set_MinClusterSize(seg_params['c_min_size'])
     ec.set_MaxClusterSize(seg_params['c_max_size'])
     ec.set_SearchMethod(tree)
     cluster_indices = ec.Extract()
-    return cluster_indices    
+    return cluster_indices
+
 
 def extract_segments(scan, seg_params):
     if seg_params['visualize']:
@@ -29,6 +36,14 @@ def extract_segments(scan, seg_params):
     # PointCloud转化为pcl 格式类型PCD
     # 点云进行平面分割
     cloud = pcl.PointCloud(scan)
+    points_raw = np.zeros((cloud.size, 3), dtype=np.float32)
+
+    for i, indices in enumerate(cloud):
+        # print(indices)
+        points_raw[i][0] = indices[0]
+        points_raw[i][1] = indices[1]
+        points_raw[i][2] = indices[2]
+
     seg = cloud.make_segmenter_normals(ksearch=50)
     seg.set_optimize_coefficients(True)
     # SACMODEL_NORMAL_PLANE平面标志 SACMODEL_CYLINDER柱面标志
@@ -52,7 +67,7 @@ def extract_segments(scan, seg_params):
     crop_xyz = crop_xyz[crop_xyz[:, -1] > -seg_params['g_height']]
     # Voxel filter (optional)  点云体素化
     ds_f = seg_params['ds_factor']
-    if ds_f > 0.01:    
+    if ds_f > 0.01:
         cloud = pcl.PointCloud(crop_xyz)
         vg = cloud.make_voxel_grid_filter()
         vg.set_leaf_size(ds_f, ds_f, ds_f)
@@ -67,11 +82,12 @@ def extract_segments(scan, seg_params):
     cluster_indices = extract_cluster_indices(cloud_filtered, seg_params)
 
     if seg_params['visualize']:
-        print('cluster_indices : ' , np.shape(cluster_indices))
+        print('cluster_indices : ', np.shape(cluster_indices))
 
     segments = []
     points_database = []
     colours_database = []
+    point_with_color_database = []
     init = False
     # 
     for j, indices in enumerate(cluster_indices):
@@ -83,43 +99,196 @@ def extract_segments(scan, seg_params):
             points[k][2] = cloud_filtered[indice][2]
 
         # Additional filtering step to remove flat(ground-plane) segments 去除平坦的段平面
-        x_diff = (max(points[:,0]) - min(points[:,0]))
-        y_diff = (max(points[:,1]) - min(points[:,1]))
-        z_diff = (max(points[:,2]) - min(points[:,2]))
+        x_diff = (max(points[:, 0]) - min(points[:, 0]))
+        y_diff = (max(points[:, 1]) - min(points[:, 1]))
+        z_diff = (max(points[:, 2]) - min(points[:, 2]))
         # 识别分割出的段中的便平面，识别方法最大的x或y差值/z差值，小于预设阈值，即俯仰角或滚转角小于一定角度
         # 分割出的段就是扁平的面
-        if (not seg_params['filter_flat_seg']) or (max(x_diff,y_diff)/z_diff < seg_params['horizontal_ratio']):
+        if (not seg_params['filter_flat_seg']) or (max(x_diff, y_diff) / z_diff < seg_params['horizontal_ratio']):
             segments.append(points)
-            colour = np.random.random_sample((3))
+            colour = np.random.random_sample((1))
             if init:
                 # vstack 按行串联数组
                 points_database = np.vstack((points_database, points))
                 colour = np.tile(colour, (len(indices), 1))
                 colours_database = np.vstack((colours_database, colour))
+
             else:
                 points_database = points
                 colour = np.tile(colour, (len(indices), 1))
                 colours_database = colour
                 init = True
-
+    point_with_color_database = np.hstack((points_database, colours_database))
     if seg_params['visualize']:
-        visualize_scan_open3d(points_database, 'seqment clouds', colours_database)    
+        visualize_scan_open3d(points_database, 'seqment clouds', colours_database)
+
+    proj_range_seg, _, color_segmets, _ = range_projection_colorseg(point_with_color_database)
+    proj_range_raw, _, _ = range_projection_points(points_raw)
+
+    fig, axs = plt.subplots(3, figsize=(6, 4))
+    axs[0].set_title('range_data_raw')
+    axs[0].imshow(proj_range_raw)
+    axs[0].set_axis_off()
+
+    axs[1].set_title('range_data_segments')
+    axs[1].imshow(proj_range_seg)
+    axs[1].set_axis_off()
+
+    axs[2].set_title('range_data_segments_colored')
+    axs[2].imshow(color_segmets)
+    axs[2].set_axis_off()
+
+    plt.suptitle('Difference between raw and segments data')
+    plt.show()
 
     # visualiza_scan_clouds(cloud, cloud_filtered, points_database, colours_database)
     return segments
+
+
+def extract_segments_2d(scan, seg_params):
+    if seg_params['visualize']:
+        visualize_scan_open3d(scan, 'raw clouds')
+
+    # Ground Plane Removal
+    # PointCloud转化为pcl 格式类型PCD
+    # 点云进行平面分割
+    cloud = pcl.PointCloud(scan)
+    points_raw = np.zeros((cloud.size, 3), dtype=np.float32)
+
+    for i, indices in enumerate(cloud):
+        # print(indices)
+        points_raw[i][0] = indices[0]
+        points_raw[i][1] = indices[1]
+        points_raw[i][2] = indices[2]
+
+    seg = cloud.make_segmenter_normals(ksearch=50)
+    seg.set_optimize_coefficients(True)
+    # SACMODEL_NORMAL_PLANE平面标志 SACMODEL_CYLINDER柱面标志
+    seg.set_model_type(pcl.SACMODEL_NORMAL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    seg.set_distance_threshold(seg_params['g_dist_thresh'])
+    seg.set_normal_distance_weight(seg_params['g_normal_dist_weight'])
+    seg.set_max_iterations(100)
+    indices, coefficients = seg.segment()
+    # print(indices)
+    # crop_xyz 除去地面的点云
+    crop_xyz = np.asarray(cloud)
+    before_clouds = crop_xyz.size
+    # print(before_clouds)
+    for k, indice in enumerate(indices):
+        # k 滤除地面后点云重新开始的序号
+        # indice 保留的点云原始序号
+        # 将提取的全部地面点云的z值设置为-20
+        crop_xyz[indice][2] = -20.0
+    # 根据传感器的高度进一步筛选,传感器为坐标轴中心，所以地面上物体为 z > sensor_height
+    crop_xyz = crop_xyz[crop_xyz[:, -1] > -seg_params['g_height']]
+    # Voxel filter (optional)  点云体素化
+    ds_f = seg_params['ds_factor']
+    if ds_f > 0.01:
+        cloud = pcl.PointCloud(crop_xyz)
+        vg = cloud.make_voxel_grid_filter()
+        vg.set_leaf_size(ds_f, ds_f, ds_f)
+        cloud_filtered = vg.filter()
+    else:
+        cloud_filtered = pcl.PointCloud(crop_xyz)
+
+    if seg_params['visualize']:
+        visualize_scan_open3d(cloud_filtered, 'filtered cloud')
+
+    # Euclidean Cluster Extraction
+    cluster_indices = extract_cluster_indices(cloud_filtered, seg_params)
+
+    if seg_params['visualize']:
+        print('cluster_indices : ', np.shape(cluster_indices))
+
+    points_database = []
+    colours_database = []
+    centroid_database = []
+    init = False
+    #
+    for j, indices in enumerate(cluster_indices):
+        points = np.zeros((len(indices), 3), dtype=np.float32)
+        # 存储每个段中的点
+        for k, indice in enumerate(indices):
+            points[k][0] = cloud_filtered[indice][0]
+            points[k][1] = cloud_filtered[indice][1]
+            points[k][2] = cloud_filtered[indice][2]
+
+        # Additional filtering step to remove flat(ground-plane) segments 去除平坦的段平面
+        x_diff = (max(points[:, 0]) - min(points[:, 0]))
+        y_diff = (max(points[:, 1]) - min(points[:, 1]))
+        z_diff = (max(points[:, 2]) - min(points[:, 2]))
+        # 识别分割出的段中的便平面，识别方法最大的x或y差值/z差值，小于预设阈值，即俯仰角或滚转角小于一定角度
+        # 分割出的段就是扁平的面
+        #
+        if (not seg_params['filter_flat_seg']) or (max(x_diff, y_diff) / z_diff < seg_params['horizontal_ratio']):
+            # ji suan mei ge duan de zhi xin
+            centroid = np.mean(points, axis=0)
+            depth = np.linalg.norm(centroid, 2)
+            depth = depth[(depth > 0) & (depth < 50)]
+            depth = np.array([depth])
+            colour = np.random.random_sample((1))
+            if init:
+                # vstack 按行串联数组
+                points_database = np.vstack((points_database, points))
+                colour = np.tile(colour, (len(indices), 1))
+                colours_database = np.vstack((colours_database, colour))
+                depth = np.tile(depth, (len(indices), 1))
+                centroid_database = np.vstack((centroid_database, depth))
+
+            else:
+                points_database = points
+                colour = np.tile(colour, (len(indices), 1))
+                colours_database = colour
+                depth = np.tile(depth, (len(indices), 1))
+                centroid_database = depth
+                init = True
+
+    point_with_color_database = np.hstack((points_database, colours_database,centroid_database))
+
+    print(point_with_color_database.size)
+    # if seg_params['visualize']:
+    #     visualize_scan_open3d(points_database, 'seqment clouds', colours_database)
+    #
+    proj_range_seg, _, color_segmets, _, proj_centroid_range = range_projection_colorseg(point_with_color_database)
+    proj_range_raw, _, _ = range_projection_points(points_raw)
+    #
+    fig, axs = plt.subplots(4, figsize=(6, 4))
+    axs[0].set_title('range_data_raw')
+    axs[0].imshow(proj_range_raw)
+    axs[0].set_axis_off()
+
+    axs[1].set_title('range_data_segments')
+    axs[1].imshow(proj_range_seg)
+    axs[1].set_axis_off()
+
+    axs[2].set_title('range_data_segments_colored')
+    axs[2].imshow(color_segmets)
+    axs[2].set_axis_off()
+
+    axs[3].set_title('centroid_range_data_segments')
+    axs[3].imshow(proj_centroid_range)
+    axs[3].set_axis_off()
+
+    plt.suptitle('Difference between raw and segments data')
+    plt.show()
+
+    # visualiza_scan_clouds(cloud, cloud_filtered, points_database, colours_database)
+    return proj_range_raw
 
 def get_segments(scan, seg_params):
     segments = extract_segments(scan, seg_params)
 
     # Handling rare degenerate scenes 处理稀有的退化场景
     c_tolerence = seg_params['c_tolerence']
-    while seg_params['enforce_min_seg_count'] and len(segments) < seg_params['min_seg_count']: 
+    while seg_params['enforce_min_seg_count'] and len(segments) < seg_params['min_seg_count']:
         seg_params['c_tolerence'] -= 0.01
         if seg_params['c_tolerence'] < 0.01:
             break
         segments = extract_segments(scan, seg_params)
     seg_params['c_tolerence'] = c_tolerence
     return segments
+
 
 #####################################################################################
 # Test
@@ -128,11 +297,11 @@ def get_segments(scan, seg_params):
 
 if __name__ == "__main__":
 
-    import glob 
+    import glob
     import yaml
     from utils.kitti_dataloader import yield_bin_scans
     from utils.misc_utils import save_pickle
-    
+
     seq = '07'
 
     cfg_file = open('config.yml', 'r')
@@ -143,19 +312,21 @@ if __name__ == "__main__":
     sequence_path = basedir + 'sequences/' + seq + '/'
     bin_files = sorted(glob.glob(os.path.join(
         sequence_path, 'velodyne', '*.bin')))
-    
-    scans = yield_bin_scans(bin_files)    
+
+    scans = yield_bin_scans(bin_files)
     segments_database = []
 
     # scan = next(scans)
     # cloud = pcl.PointCloud(scan[:, :-1])
+    # # only get x y z, remove identity
     # segments = extract_segments(scan[:, :-1], seg_params)
-    for i in range(10): 
+
+    for i in range(10):
         scan = next(scans)
-        segments = extract_segments(scan[:, :-1], seg_params)
+        segments = extract_segments_2d(scan[:, :-1], seg_params)
         print('Extracted segments: ', np.shape(segments))
         segments_database.append(segments)
-    
-    save_dir = cfg_params['paths']['save_dir'] + seq
-    save_pickle(segments_database, save_dir +
-                '/segments_database.pickle')
+
+    # save_dir = cfg_params['paths']['save_dir'] + seq
+    # save_pickle(segments_database, save_dir +
+    #             '/segments_database.pickle')
--- utils/kitti_dataloader.py
+++ utils/kitti_dataloader.py
@@ -10,18 +10,20 @@ import open3d as o3d
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from utils.misc_utils import *
 
+
 #####################################################################################
 # Load poses
 #####################################################################################
 
 def transfrom_cam2velo(Tcam):
-    R = np.array([ 7.533745e-03, -9.999714e-01, -6.166020e-04, 1.480249e-02, 7.280733e-04,
-        -9.998902e-01, 9.998621e-01, 7.523790e-03, 1.480755e-02
-    ]).reshape(3, 3)
+    R = np.array([7.533745e-03, -9.999714e-01, -6.166020e-04, 1.480249e-02, 7.280733e-04,
+                  -9.998902e-01, 9.998621e-01, 7.523790e-03, 1.480755e-02
+                  ]).reshape(3, 3)
     t = np.array([-4.069766e-03, -7.631618e-02, -2.717806e-01]).reshape(3, 1)
     cam2velo = np.vstack((np.hstack([R, t]), [0, 0, 0, 1]))
 
     return Tcam @ cam2velo
+
 
 def load_poses_from_txt(file_name):
     """
@@ -38,7 +40,7 @@ def load_poses_from_txt(file_name):
         withIdx = len(line_split) == 13
         for row in range(3):
             for col in range(4):
-                P[row, col] = line_split[row*4 + col + withIdx]
+                P[row, col] = line_split[row * 4 + col + withIdx]
         if withIdx:
             frame_idx = line_split[0]
         else:
@@ -50,14 +52,15 @@ def load_poses_from_txt(file_name):
 
 def get_delta_pose(transforms):
     rel_transforms = []
-    for i in range(len(transforms)-1):
+    for i in range(len(transforms) - 1):
         w_T_p1 = transforms[i]
-        w_T_p2 = transforms[i+1]
+        w_T_p2 = transforms[i + 1]
 
         p1_T_w = T_inv(w_T_p1)
         p1_T_p2 = np.matmul(p1_T_w, w_T_p2)
         rel_transforms.append(p1_T_p2)
     return rel_transforms
+
 
 #####################################################################################
 # Load scans
@@ -78,14 +81,16 @@ def yield_bin_scans(bin_files):
     for file in bin_files:
         yield load_bin_scan(file)
 
-def visualize_scan_open3d(ptcloud_xyz, tittle = 'clouds', colors = []):
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(ptcloud_xyz)
-        if colors != []:
-            pcd.colors = o3d.utility.Vector3dVector(colors)
-        o3d.visualization.draw_geometries([pcd], tittle)
 
-def visualiza_scan_clouds(raw, filtered, seg, colors = []):
+def visualize_scan_open3d(ptcloud_xyz, tittle='clouds', colors=[]):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(ptcloud_xyz)
+    if colors != []:
+        pcd.colors = o3d.utility.Vector3dVector(colors)
+    o3d.visualization.draw_geometries([pcd], tittle)
+
+
+def visualiza_scan_clouds(raw, filtered, seg, colors=[]):
     pcd_raw = o3d.geometry.PointCloud()
     pcd_raw.points = o3d.utility.Vector3dVector(raw)
     pcd_raw.paint_uniform_color([1, 0, 0])
@@ -96,6 +101,7 @@ def visualiza_scan_clouds(raw, filtered, seg, colors = []):
     pcd_seg.points = o3d.utility.Vector3dVector(seg)
     pcd_seg.colors = o3d.utility.Vector3dVector(colors)
     o3d.visualization.draw_geometries([pcd_raw, pcd_filtered, pcd_seg])
+
 
 def visualize_sequence_open3d(bin_files, n_scans):
     """Visualize scans using Open3D"""
@@ -110,6 +116,189 @@ def visualize_sequence_open3d(bin_files, n_scans):
 
 
 #####################################################################################
+# make points projection
+#####################################################################################
+
+def range_projection_points(current_vertex, fov_up=3.0, fov_down=-25.0, proj_H=64, proj_W=900, max_range=50):
+    # OverlapNet initial parameters
+    # fov_up=3.0, fov_down=-25.0, proj_H=64, proj_W=900, max_range=50
+    """ Project a pointcloud into a spherical projection, range image.
+      Args:
+        current_vertex: raw point clouds 矩阵构成： 第一列 x 第二列 y 第三列z
+      Returns:
+        proj_range: projected range image with depth, each pixel contains the corresponding depth   带深度的投影范围图像，每个像素包含深度
+        proj_vertex: each pixel contains the corresponding point (x, y, z, 1)                       每个像素包含对应点xyz
+        proj_intensity: each pixel contains the corresponding intensity                             每个像素包含对应点的反射强度
+        proj_idx: each pixel contains the corresponding index of the point in the raw point cloud   每个像素包含对应点的原始点云索引
+  """
+    # laser parameters
+    fov_up = fov_up / 180.0 * np.pi  # field of view up in radians 弧度制上视角
+    fov_down = fov_down / 180.0 * np.pi  # field of view down in radians 弧度制下视角
+    fov = abs(fov_down) + abs(fov_up)  # get field of view total in radians 整个视角
+
+    # get depth of all points
+    depth = np.linalg.norm(current_vertex[:, :3], 2, axis=1)  # 计算每个点云的距离,计算矩阵前三列的2范数
+    current_vertex = current_vertex[(depth > 0) & (depth < max_range)]  # get rid of [0, 0, 0] points
+    depth = depth[(depth > 0) & (depth < max_range)]
+
+    # get scan components
+    scan_x = current_vertex[:, 0]
+    scan_y = current_vertex[:, 1]
+    scan_z = current_vertex[:, 2]
+
+    # get angles of all points
+    # 计算偏航角和俯仰角
+    # 偏航角为偏离x轴方向，运动正向为x轴正向，y轴负向，arctan(y/x)
+    yaw = -np.arctan2(scan_y, scan_x)
+    # 俯仰角为z轴和深度的反正弦，arcsin(z/d)
+    pitch = np.arcsin(scan_z / depth)
+
+    # get projections in image coords
+    # 按照偏航角和俯仰角进行球面投影
+    # 偏航角在-180～180，进行非负化和归一化处理
+    proj_x = 0.5 * (yaw / np.pi + 1.0)  # in [0.0, 1.0]
+    # 加上最低视角进行非负化，再除以总视角进行归一化，再被1减，符合图像坐标系
+    # 如果激光雷达的扫描线数不是均匀的，则投影也不是均匀的，需要注意
+    proj_y = 1.0 - (pitch + abs(fov_down)) / fov  # in [0.0, 1.0]
+
+    # scale to image size using angular resolution
+    # 恢复尺度
+    proj_x *= proj_W  # in [0.0, W]
+    proj_y *= proj_H  # in [0.0, H]
+
+    # round and clamp for use as index
+    proj_x = np.floor(proj_x)  # 对整个投影向下取整
+    proj_x = np.minimum(proj_W - 1, proj_x)  # 确保上下界
+    proj_x = np.maximum(0, proj_x).astype(np.int32)  # in [0,W-1]
+
+    proj_y = np.floor(proj_y)
+    proj_y = np.minimum(proj_H - 1, proj_y)
+    proj_y = np.maximum(0, proj_y).astype(np.int32)  # in [0,H-1]
+
+    # order in decreasing depth
+    order = np.argsort(depth)[::-1]  # 获得按距离的降序排列的序号
+    depth = depth[order]
+
+
+    proj_y = proj_y[order]  # 按照距离远近降序排列
+    proj_x = proj_x[order]
+
+    scan_x = scan_x[order]
+    scan_y = scan_y[order]
+    scan_z = scan_z[order]  # 将xyz的顺序与深度顺序对齐
+
+    indices = np.arange(depth.shape[0])
+    indices = indices[order]
+    # 初始化伪图像
+    proj_range = np.full((proj_H, proj_W), -1,
+                         dtype=np.float32)  # [H,W] range (-1 is no data)
+    proj_vertex = np.full((proj_H, proj_W, 4), -1,
+                          dtype=np.float32)  # [H,W] index (-1 is no data)
+    proj_idx = np.full((proj_H, proj_W), -1,
+                       dtype=np.int32)  # [H,W] index (-1 is no data)
+
+    # 按照距离顺序进行图像点赋值，因为分辨率的压缩，会存在取整后重复的点，按照距离降序赋值的话，同一点选择距离最近的值作为代表
+    proj_range[proj_y, proj_x] = depth
+    proj_vertex[proj_y, proj_x] = np.array([scan_x, scan_y, scan_z, np.ones(len(scan_x))]).T
+    proj_idx[proj_y, proj_x] = indices
+
+    return proj_range, proj_vertex, proj_idx
+
+
+def range_projection_colorseg(current_vertex, fov_up=3.0, fov_down=-25.0, proj_H=64, proj_W=900, max_range=50):
+    # OverlapNet initial parameters
+    # fov_up=3.0, fov_down=-25.0, proj_H=64, proj_W=900, max_range=50
+    """ Project a pointcloud into a spherical projection, range image.
+      Args:
+        current_vertex: raw point clouds 矩阵构成： 第一列 x 第二列 y 第三列z 第四列反射强度
+      Returns:
+        proj_range: projected range image with depth, each pixel contains the corresponding depth   带深度的投影范围图像，每个像素包含深度
+        proj_vertex: each pixel contains the corresponding point (x, y, z, 1)                       每个像素包含对应点xyz
+        proj_intensity: each pixel contains the corresponding intensity                             每个像素包含对应点的反射强度
+        proj_idx: each pixel contains the corresponding index of the point in the raw point cloud   每个像素包含对应点的原始点云索引
+  """
+    # laser parameters
+    fov_up = fov_up / 180.0 * np.pi  # field of view up in radians 弧度制上视角
+    fov_down = fov_down / 180.0 * np.pi  # field of view down in radians 弧度制下视角
+    fov = abs(fov_down) + abs(fov_up)  # get field of view total in radians 整个视角
+
+    # get depth of all points
+    depth = np.linalg.norm(current_vertex[:, :3], 2, axis=1)  # 计算每个点云的距离,计算矩阵前三列的2范数
+    current_vertex = current_vertex[(depth > 0) & (depth < max_range)]  # get rid of [0, 0, 0] points
+    depth = depth[(depth > 0) & (depth < max_range)]
+
+    # get scan components
+    scan_x = current_vertex[:, 0]
+    scan_y = current_vertex[:, 1]
+    scan_z = current_vertex[:, 2]
+    color = current_vertex[:, 3]
+    centroid = current_vertex[:, 4]
+    # print("color", color)
+    # get angles of all points
+    # 计算偏航角和俯仰角
+    # 偏航角为偏离x轴方向，运动正向为x轴正向，y轴负向，arctan(y/x)
+    yaw = -np.arctan2(scan_y, scan_x)
+    # 俯仰角为z轴和深度的反正弦，arcsin(z/d)
+    pitch = np.arcsin(scan_z / depth)
+
+    # get projections in image coords
+    # 按照偏航角和俯仰角进行球面投影
+    # 偏航角在-180～180，进行非负化和归一化处理
+    proj_x = 0.5 * (yaw / np.pi + 1.0)  # in [0.0, 1.0]
+    # 加上最低视角进行非负化，再除以总视角进行归一化，再被1减，符合图像坐标系
+    # 如果激光雷达的扫描线数不是均匀的，则投影也不是均匀的，需要注意
+    proj_y = 1.0 - (pitch + abs(fov_down)) / fov  # in [0.0, 1.0]
+
+    # scale to image size using angular resolution
+    # 恢复尺度
+    proj_x *= proj_W  # in [0.0, W]
+    proj_y *= proj_H  # in [0.0, H]
+
+    # round and clamp for use as index
+    proj_x = np.floor(proj_x)  # 对整个投影向下取整
+    proj_x = np.minimum(proj_W - 1, proj_x)  # 确保上下界
+    proj_x = np.maximum(0, proj_x).astype(np.int32)  # in [0,W-1]
+
+    proj_y = np.floor(proj_y)
+    proj_y = np.minimum(proj_H - 1, proj_y)
+    proj_y = np.maximum(0, proj_y).astype(np.int32)  # in [0,H-1]
+
+    # order in decreasing depth
+    order = np.argsort(depth)[::-1]  # 获得按距离的降序排列的序号
+    depth = depth[order]
+
+    color = color[order]  # 反射强度和距离相关，一样的序号
+    centroid = centroid[order]
+    # print("here color type:", type(color))
+    proj_y = proj_y[order]  # 按照距离远近降序排列
+    proj_x = proj_x[order]
+
+    scan_x = scan_x[order]
+    scan_y = scan_y[order]
+    scan_z = scan_z[order]  # 将xyz的顺序与深度顺序对齐
+
+    indices = np.arange(depth.shape[0])
+    indices = indices[order]
+    # 初始化伪图像
+    proj_range = np.full((proj_H, proj_W), -1,
+                         dtype=np.float32)  # [H,W] range (-1 is no data)
+    proj_vertex = np.full((proj_H, proj_W, 4), -1,
+                          dtype=np.float32)  # [H,W] index (-1 is no data)
+    proj_idx = np.full((proj_H, proj_W), -1,
+                       dtype=np.int32)  # [H,W] index (-1 is no data)
+    proj_color = np.full((proj_H, proj_W), 0,
+                             dtype=np.float32)  # [H,W] index (-1 is no data)
+    proj_centroid = np.full((proj_H, proj_W), 0,
+                         dtype=np.float32)  # [H,W] index (-1 is no data)
+    # 按照距离顺序进行图像点赋值，因为分辨率的压缩，会存在取整后重复的点，按照距离降序赋值的话，同一点选择距离最近的值作为代表
+    # print("depth", depth)
+    proj_range[proj_y, proj_x] = depth
+    proj_vertex[proj_y, proj_x] = np.array([scan_x, scan_y, scan_z, np.ones(len(scan_x))]).T
+    proj_idx[proj_y, proj_x] = indices
+    proj_color[proj_y, proj_x] = color
+    proj_centroid[proj_y, proj_x] = centroid
+    return proj_range, proj_vertex, proj_color, proj_idx, proj_centroid
+#####################################################################################
 # Load timestamps
 #####################################################################################
 
@@ -120,10 +309,11 @@ def load_timestamps(file_name):
     stimes_list = file1.readlines()
     s_exp_list = np.asarray([float(t[-4:-1]) for t in stimes_list])
     times_list = np.asarray([float(t[:-2]) for t in stimes_list])
-    times_listn = [times_list[t] * (10**(s_exp_list[t]))
+    times_listn = [times_list[t] * (10 ** (s_exp_list[t]))
                    for t in range(len(times_list))]
     file1.close()
     return times_listn
+
 
 #####################################################################################
 # Test
@@ -150,8 +340,8 @@ if __name__ == "__main__":
         _, positions = load_poses_from_txt(poses_file[0])
         print('seq: ', sequence, 'len', len(positions))
 
-        axs[i//6, i % 6].plot(positions[:,0], positions[:,1])
-        axs[i//6, i % 6].set_title('seq: ' + sequence + 'len' + str(len(positions)))
+        axs[i // 6, i % 6].plot(positions[:, 0], positions[:, 1])
+        axs[i // 6, i % 6].set_title('seq: ' + sequence + 'len' + str(len(positions)))
 
     plt.show()
 
